f5cd76a89aca4ce1bbf1eb324490f6f17f2b25cf
quinone <eoghanm.quinn@gmail.com>
2024-01-13 14:02:05 +0000

    Function adds lower letters to set and returns length == 26

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,3 +1,6 @@
 def is_pangram(sentence):
-    
-    
+    alphabet = set()
+    for letter in sentence:
+        if letter.isalpha():
+            alphabet.add(letter.lower())    
+    return len(alphabet) == 26
