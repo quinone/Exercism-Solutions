231df2621e63962cd2bf28e31d63642b5e259a99
quinone <eoghanm.quinn@gmail.com>
2024-01-17 10:24:29 +0000

    completed is_isogram()

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,9 @@
 def is_isogram(string):
-    pass
+    letter_list = set()
+    for letter in string:
+        if letter.lower() in letter_list:
+            return False
+        if letter.isalpha():
+            letter_list.add(letter.lower())
+    return True
+
