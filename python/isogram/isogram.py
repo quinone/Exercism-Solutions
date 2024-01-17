bd53db45e84263836d7273001d3b268308c76325
quinone <eoghanm.quinn@gmail.com>
2024-01-17 10:52:44 +0000

    removed unsed code

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,8 +1,4 @@
 def is_isogram(string):
     letter_list = [letter.lower() for letter in string if letter.isalpha()]
-        #if letter.lower() in letter_list:
-        #    return False
-        #if letter.isalpha():
-        #    letter_list.add(letter.lower())
     return len(letter_list) == len(set(letter_list))
 
