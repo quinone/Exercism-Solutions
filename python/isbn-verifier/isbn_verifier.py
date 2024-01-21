a84c241fb6786a6fc4227496f7ea1f303ac62cba
quinone <eoghanm.quinn@gmail.com>
2024-01-21 14:28:40 +0000

    completed is_valid()

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,25 @@
 def is_valid(isbn):
-    pass
+    
+    isbn = isbn.replace('-', '')
+    new_isbn = isbn.lower()
+    if len(new_isbn)!=10:
+        return False
+    if new_isbn.isalpha() and not (new_isbn[-1]=='x'):
+        return False
+    total = 0
+    d = 10
+    for index in range(9):
+        value = new_isbn[index]
+        if value.isalpha():
+            return False
+        number = int(value)
+        total += d * number
+        d-=1
+    if new_isbn[-1] == 'x':
+        total += 10
+    elif new_isbn[-1].isalpha():
+        return False
+    else:
+        total += int(new_isbn[-1])
+    return total % 11 == 0
+        
