b8d3989f8336e314f3a769a66c366d21c812cbc8
quinone <eoghanm.quinn@gmail.com>
2024-01-21 14:40:07 +0000

    moved check if x is last into loop through isbn

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,25 +1,19 @@
 def is_valid(isbn):
-    
     isbn = isbn.replace('-', '')
     new_isbn = isbn.lower()
     if len(new_isbn)!=10:
         return False
-    if new_isbn.isalpha() and not (new_isbn[-1]=='x'):
-        return False
+
     total = 0
     d = 10
-    for index in range(9):
+    for index in range(10):
         value = new_isbn[index]
         if value.isalpha():
-            return False
+            if value == 'x' and index == 9:
+                value = 10
+            else:
+                return False    
         number = int(value)
         total += d * number
         d-=1
-    if new_isbn[-1] == 'x':
-        total += 10
-    elif new_isbn[-1].isalpha():
-        return False
-    else:
-        total += int(new_isbn[-1])
     return total % 11 == 0
-        
