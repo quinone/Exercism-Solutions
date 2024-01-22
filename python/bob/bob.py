55efca65e61555fbd60bf6e8b14f88f689288e97
quinone <eoghanm.quinn@gmail.com>
2024-01-22 13:16:46 +0000

    completed response

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,13 @@
-def response(hey_bob):
-    pass
+def response(hey_bob):    
+    bob = hey_bob.strip().replace(',', '')
+    if bob=='':
+        return "Fine. Be that way!"
+    if bob[-1]=='?':
+        if bob[:-1].isupper():
+            return "Calm down, I know what I'm doing!"
+        return "Sure."
+    if bob[:-1].isupper():
+        return "Whoa, chill out!"
+
+    return "Whatever."
+
