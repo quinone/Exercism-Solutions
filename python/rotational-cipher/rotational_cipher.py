c10c0ed0884e3c313dfd7e6d3f1c7c66838f2ca5
quinone <eoghanm.quinn@gmail.com>
2024-01-24 10:23:20 +0000

    removed unneeded if else

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,15 +1,10 @@
 def rotate(text, key):
-    cipher_text = ''
+    cipher_text = ""
     for letter in text:
-        if letter.isalpha():
-            letter_number = ord(letter)
-            if 65 <= letter_number <= 90:
-                letter_key = (letter_number - 65 + key) % 26
-                cipher_letter = chr(letter_key + 65)
-            if 97 <= letter_number <= 122:
-                letter_key = (letter_number - 97 + key) % 26
-                cipher_letter = chr(letter_key + 97)
-            cipher_text = cipher_text + cipher_letter
-        else:
-            cipher_text = cipher_text + letter
+        letter_number = ord(letter)
+        if 65 <= letter_number <= 90:
+            letter_number = ((letter_number - 65 + key) % 26) + 65
+        if 97 <= letter_number <= 122:
+            letter_number = ((letter_number - 97 + key) % 26) + 97
+        cipher_text += chr(letter_number)
     return cipher_text
