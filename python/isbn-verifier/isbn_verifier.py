b855a77386d406f9e8597c66fe4a38490191cba3
quinone <eoghanm.quinn@gmail.com>
2024-01-24 09:28:11 +0000

    changed lambda function to zip, replaced unpacking list to just pass list to list()

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,16 +1,17 @@
 def is_valid(isbn):
-    isbn = isbn.replace('-', '')
-    isbn = [*isbn]
-    if len(isbn)!=10:
+    isbn = list(isbn.replace("-", ""))
+    if len(isbn) != 10:
         return False
-    
-    if isbn[-1] == 'X':
-        isbn[-1] = '10'
+
+    if isbn[-1] == "X":
+        isbn[-1] = "10"
 
     if any(number.isalpha() for number in isbn):
         return False
-    
-    isbn = [int(digit) for digit in isbn]
-    total = sum(map(lambda digit, index: digit * index, isbn, range(10, 0, -1)))
 
-    return total % 11 == 0
+    return sum(int(digit) * i for digit, i in zip(isbn, range(10, 0, -1))) % 11 == 0
+
+    #return (
+    #   sum(map(lambda digit, index: int(digit) * index, isbn, range(10, 0, -1))) % 11
+    #   == 0
+    #)
