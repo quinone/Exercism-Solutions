9601c3301439d35ea06f3ec7e72722381520e7d0
quinone <eoghanm.quinn@gmail.com>
2024-01-27 15:10:17 +0000

    completed armstrong number

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,7 @@
 def is_armstrong_number(number):
-    pass
+    number_string = str(number)
+    number_list = list(number_string)
+    total = 0
+    for digit in number_list:
+        total+= pow(int(digit), len(number_list))
+    return total == number
