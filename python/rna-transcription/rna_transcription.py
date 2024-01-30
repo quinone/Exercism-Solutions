a0b454b972bee4f4b591fa15162d709d727a006d
quinone <eoghanm.quinn@gmail.com>
2024-01-30 09:46:40 +0000

    Refactor: Uses maketrans and translate

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,12 +1,4 @@
+dna_to_rna = str.maketrans("ATGC", "UACG")
+
 def to_rna(dna_strand):
-    rna = ''
-    for nucleo in dna_strand:
-        if nucleo == 'A':
-            rna+='U'
-        elif nucleo == 'T':
-            rna+='A'
-        elif nucleo == 'G':
-            rna+='C'
-        elif nucleo == 'C':
-            rna+='G'
-    return rna
+    return dna_strand.translate(dna_to_rna)
