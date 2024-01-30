7897aa6866e6a0726cca9cda0ccc8987c37c73cd
quinone <eoghanm.quinn@gmail.com>
2024-01-30 10:43:42 +0000

    Refactor: created tran_pig function and compressed translate to call tran_pig on text.split()

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,22 +1,23 @@
+vowel_sound = {"a", "e", "i", "o", "u"}
+
+
 def translate(text):
-    vowel_sound = ("a", "e", "i", "o", "u")
-    text = text.split()
-    translation = []
-    for word in text:
-        if word[0] in vowel_sound or word[:2] in ("xr", "yt"):
-            word = word
-        elif word.startswith("qu"):
-            word = word[2:] + "qu"
-        else: 
-            if word[1] in ("h", "c", "t"):
-                if word[2] in ("h", "r"):
-                    word = word[3:] + word[:3]
-                else:
-                    word = word[2:] + word[:2]
-            elif word[1:3] == "qu":
+    return " ".join([trans_pig(word) for word in text.split()])
+
+
+def trans_pig(word):
+    if word[0] in vowel_sound or word[:2] in ("xr", "yt"):
+        word = word
+    elif word.startswith("qu"):
+        word = word[2:] + "qu"
+    else:
+        if word[1] in ("h", "c", "t"):
+            if word[2] in ("h", "r"):
                 word = word[3:] + word[:3]
             else:
-                word = word[1:] + word[0]
-        translation.append(word + "ay")
-
-    return " ".join(translation)
+                word = word[2:] + word[:2]
+        elif word[1:3] == "qu":
+            word = word[3:] + word[:3]
+        else:
+            word = word[1:] + word[0]
+    return word + "ay"
