99226457433d9bed24c7ffae9c734ecdffc678df
quinone <eoghanm.quinn@gmail.com>
2024-01-30 09:55:21 +0000

    Refactor: removed unused else and const_sound tuple

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,26 +1,22 @@
 def translate(text):
-    vowel_sound = ('a', 'e','i','o','u')
+    vowel_sound = ("a", "e", "i", "o", "u")
     text = text.split()
-    translation=[]
+    translation = []
     for word in text:
-        if word[0] in vowel_sound or word[:2] in ('xr', 'yt'):
-            word+='ay'
-        elif word.startswith('qu'):
-            word= word[2:] + 'quay'
-        elif word[0] in ('w', 'r', 't', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k',
-                              'l', 'z', 'c', 'v', 'b', 'n', 'm', 'x', 'y'):
-            if word[1] in ('h', 'c','t'):
-                if word[2] in ('h', 'r'):
-                    word = word[3:]+word[:3]+'ay'
+        if word[0] in vowel_sound or word[:2] in ("xr", "yt"):
+            word = word
+        elif word.startswith("qu"):
+            word = word[2:] + "qu"
+        else: 
+            if word[1] in ("h", "c", "t"):
+                if word[2] in ("h", "r"):
+                    word = word[3:] + word[:3]
                 else:
-                    word = word[2:]+word[:2]+ 'ay'
-            elif word[1:3] == 'qu':
-                word = word[3:]+word[:3]+'ay'
-            
+                    word = word[2:] + word[:2]
+            elif word[1:3] == "qu":
+                word = word[3:] + word[:3]
             else:
-                word = word[1:] + word[0] + 'ay'    
-        else:
-            word= word[1:] + word[0] + 'ay'
-        translation.append(word)
-    
-    return ' '.join(translation)
+                word = word[1:] + word[0]
+        translation.append(word + "ay")
+
+    return " ".join(translation)
