6cfecf884b267615af147cdd30d7781925ead706
quinone <eoghanm.quinn@gmail.com>
2024-01-30 10:17:35 +0000

    completed darts

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,9 @@
 def score(x, y):
-    pass
+    squared = x**2 + y**2
+    if squared <= 1:
+        return 10
+    if squared <= 5**2:
+        return 5
+    if squared <= 10**2:
+        return 1
+    return 0
