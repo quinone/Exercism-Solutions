903fcb023009cc010c143b5f6a1d84631867fecd
quinone <eoghanm.quinn@gmail.com>
2024-02-07 19:48:04 +0000

    Completed prime-factors problem

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,14 @@
 def factors(value):
-    pass
+    prime = []
+    if value == 1:
+        return prime
+    factor = 2
+    while value / factor != 1:
+        if value % factor == 0:
+            prime.append(factor)
+            value = value / factor
+        if value % factor != 0:
+            factor += 1
+
+    prime.append(factor)
+    return prime
