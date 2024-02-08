bcb4b976fd6974b52b68474d4a9c57246354b3fb
quinone <eoghanm.quinn@gmail.com>
2024-02-08 16:15:12 +0000

    Completed flatten-array

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,4 +1,8 @@
 def flatten(iterable):
-    #result = []
-    
-    return [iter for iter in iterable]
+    flat_list = []
+    for item in iterable:
+        if type(item) == list:
+            flat_list += flatten(item)
+        elif item is not None:
+            flat_list.append(item)
+    return flat_list
