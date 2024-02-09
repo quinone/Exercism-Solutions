33f0dc282fb7b57955cb627814e2a9e8d24bc9e2
quinone <eoghanm.quinn@gmail.com>
2024-02-09 17:16:20 +0000

    Refactor: Updated isPrime() to take list of primes to improve speed

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -3,21 +3,24 @@ def prime(number):
         raise ValueError("there is no zeroth prime")
     primes = []
     position = 1
-    for n in range(number):
-        
+    for _ in range(number):
         while True:
-            position+=1
-            if isPrime(position):
+            position += 1
+            if isPrime(position, primes):
                 primes.append(position)
                 break
-            
-    return primes[-1]
-        
 
-def isPrime(number):
+    return primes[-1]
+
+
+def isPrime(number, primes):
     if number == 1:
         return False
-    for n in range(2, number):
-        if number % n == 0:
-            return False
+    if primes:
+        for n in primes:
+            if number % n == 0:
+                return False
+        for n in range(primes[-1], number):
+            if number % n == 0:
+                return False
     return True
