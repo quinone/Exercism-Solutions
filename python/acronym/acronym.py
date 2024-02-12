dbec6fa8cfac009a9a71c42ec70ceacde04bce21
quinone <eoghanm.quinn@gmail.com>
2024-02-12 17:14:20 +0000

    completed acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,12 @@
 def abbreviate(words):
-    pass
+    words = words.replace('_', '')
+    words = words.replace('-', ' ')
+    word_list = words.split(' ')
+    result = ''
+    for word in word_list:
+        if not word:
+            continue
+        upper_letter = word[0].upper()
+        result+=upper_letter
+    return result
+
