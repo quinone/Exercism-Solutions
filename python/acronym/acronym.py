deda3040ebccc5f3ec8877553df1665c5cbf11aa
quinone <eoghanm.quinn@gmail.com>
2024-02-12 17:34:42 +0000

    Refactor: condensed for loop and not None check into a generator expression while cleaning argument, removing - and _ before splitting into words to be looped over

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,12 +1,3 @@
 def abbreviate(words):
-    words = words.replace('_', '')
-    words = words.replace('-', ' ')
-    word_list = words.split(' ')
-    result = ''
-    for word in word_list:
-        if not word:
-            continue
-        upper_letter = word[0].upper()
-        result+=upper_letter
-    return result
+    return ''.join(word[0].upper() for word in words.replace('_', '').replace('-', ' ').split(' ') if word)
 
