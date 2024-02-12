5f993b9d3933e3941a1d13a5807f81dded442341
quinone <eoghanm.quinn@gmail.com>
2024-02-12 18:52:54 +0000

    completed wordy

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,12 +1,42 @@
+OPERATION = {"minus", "plus", "multiplied", "divided"}
+
+
 def answer(question):
-    parse = question.split()
+    parse = question.replace("by", " ").replace("?", "").split()
+    if len(parse) < 3:
+        raise ValueError("syntax error")
+    if parse[2].isalpha():
+        if parse[2] in OPERATION:
+            raise ValueError("syntax error")
+        raise ValueError("unknown operation")
     first_operand = int(parse[2])
-    second_operand = int(parse[-1])
-    if parse[3] == 'minus':
+    if len(parse) == 3:
+        return first_operand
+
+    if parse[3].isalpha() and parse[3] not in OPERATION:
+        raise ValueError("unknown operation")
+    if len(parse) not in (3, 5, 7):
+        raise ValueError("syntax error")
+    if parse[4].isalpha():
+        raise ValueError("syntax error")
+    second_operand = int(parse[4])
+    result = calculate(first_operand, second_operand, parse[3])
+
+    if len(parse) == 5:
+        return result
+    if parse[6].isalpha():
+        raise ValueError("syntax error")
+    third_operand = int(parse[6])
+    return calculate(result, third_operand, parse[5])
+
+
+def calculate(first_operand, second_operand, operation):
+    if operation == "minus":
         return first_operand - second_operand
-    if parse[3] == 'plus':
+    if operation == "plus":
         return first_operand + second_operand
-    if parse[3] == 'multiplied':
+    if operation == "multiplied":
         return first_operand * second_operand
-    if parse[3] == 'divided':
+    if operation == "divided":
         return first_operand / second_operand
+    raise ValueError("unknown operation")
