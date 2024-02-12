9773411df37d4b0e4c5decfd39eb9f28edd7aa06
quinone <eoghanm.quinn@gmail.com>
2024-02-12 19:51:38 +0000

    Refactor: created instruction_parse function to parse and error check question sentence

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -2,32 +2,19 @@ OPERATION = {"minus", "plus", "multiplied", "divided"}
 
 
 def answer(question):
-    parse = question.replace("by", " ").replace("?", "").split()
-    if len(parse) < 3:
-        raise ValueError("syntax error")
-    if parse[2].isalpha():
-        if parse[2] in OPERATION:
-            raise ValueError("syntax error")
-        raise ValueError("unknown operation")
-    first_operand = int(parse[2])
-    if len(parse) == 3:
+    parse = instruction_parse(question)
+    first_operand = int(parse[0])
+    if len(parse) == 1:
         return first_operand
 
-    if parse[3].isalpha() and parse[3] not in OPERATION:
-        raise ValueError("unknown operation")
-    if len(parse) not in (3, 5, 7):
-        raise ValueError("syntax error")
-    if parse[4].isalpha():
-        raise ValueError("syntax error")
-    second_operand = int(parse[4])
-    result = calculate(first_operand, second_operand, parse[3])
+    second_operand = int(parse[2])
+    result = calculate(first_operand, second_operand, parse[1])
 
-    if len(parse) == 5:
+    if len(parse) == 3:
         return result
-    if parse[6].isalpha():
-        raise ValueError("syntax error")
-    third_operand = int(parse[6])
-    return calculate(result, third_operand, parse[5])
+
+    third_operand = int(parse[4])
+    return calculate(result, third_operand, parse[3])
 
 
 def calculate(first_operand, second_operand, operation):
@@ -40,3 +27,20 @@ def calculate(first_operand, second_operand, operation):
     if operation == "divided":
         return first_operand / second_operand
     raise ValueError("unknown operation")
+
+
+def instruction_parse(sentence):
+    instruction_list = sentence.replace("by", " ").replace("?", "").split()
+    instruction_list = instruction_list[2:]
+
+    for index, instruction in enumerate(instruction_list):
+        if instruction.isalpha() and instruction not in OPERATION:
+            raise ValueError("unknown operation")
+        if (index % 2 == 0 and instruction.isalpha()) or (
+            index % 2 != 0 and instruction.isnumeric()
+        ):
+            raise ValueError("syntax error")
+
+    if not instruction_list or instruction_list[-1].isalpha():
+        raise ValueError("syntax error")
+    return instruction_list
