7a9d6ee03aaad8ee30c8d2ff6c7bb1c552792b56
quinone <eoghanm.quinn@gmail.com>
2024-02-15 18:37:10 +0000

    Refactor: change colors() to generate a list of keys from COLOR_NUMBERS dictionary

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,29 +1,20 @@
 COLOR_NUMBERS = {
-            "black":0,
-            "brown":1,
-            "red":2,
-            "orange":3,
-            "yellow":4,
-            "green":5,
-            "blue":6,
-            "violet":7,
-            "grey":8,
-            "white":9,
-        }
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+
 def color_code(color):
     return COLOR_NUMBERS.get(color)
 
 
 def colors():
-    return [
-            "black",
-            "brown",
-            "red",
-            "orange",
-            "yellow",
-            "green",
-            "blue",
-            "violet",
-            "grey",
-            "white",
-        ]
+    return list(COLOR_NUMBERS.keys())
