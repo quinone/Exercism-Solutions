3a6a6a7bc5fe06b46cad0d4eb0218bd8c3bcbe69
quinone <eoghanm.quinn@gmail.com>
2024-08-31 08:29:53 +0100

    Added roman numberals

--- python/resistor-color-trio/resistor_color_trio.py
+++ python/resistor-color-trio/resistor_color_trio.py
@@ -1,2 +1,33 @@
 def label(colors):
-    pass
+    bands = {
+        'black': 0,
+        'brown': 1,
+        'red': 2,
+        'orange': 3,
+        'yellow': 4,
+        'green': 5,
+        'blue': 6,
+        'violet': 7,
+        'grey': 8,
+        'white': 9,
+    }
+    result = []
+    for color in colors:
+        if color in bands:
+            result.append(bands.get(color))
+        
+    result[2] = result[2]*'0'
+    print(result)        
+    result = [str(band) for band in result]
+    print(result)
+    if result[2] == '000':
+        result[2] = ' kilo'
+    elif result[2] == '000000':
+        result[2] = ' mega'
+    else:
+        result[2]+=' '
+    if result[0] == '0':
+        result.remove('0')
+    result = ''.join(result) + 'ohms'
+
+    return result
