25ed00cbd1b64b89ba42b72e5928975b7b2bfc69
quinone <eoghanm.quinn@gmail.com>
2024-08-31 09:03:09 +0100

    Solved all tests in roman_numerals

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -1,3 +1,32 @@
 def roman(number):
-    pass
+    result = []
+    values = {
+        1000 : 'M',
+        900: 'CM',
+        500 : 'D',
+        100 : 'C',
+        90 : 'XC',
+        50 : 'L',
+        40: 'XL',
+        10 : 'X',
+        9: 'IX',
+        5 : 'V',
+        4: 'IV',
+        1 : 'I',
+    }
 
+    for numerical, symbol in values.items():
+        
+        while numerical <= number:
+            number = number - numerical
+            result.append(symbol)
+
+    result = ''.join(result)
+    result = result.replace('IIII','IV') 
+    result = result.replace('VIIII','IX')
+    result = result.replace('XXXX','XL')
+    result = result.replace('LLLL','XC')
+    result = result.replace('CCCC','CD')
+    result = result.replace('DDDD','CM')
+                
+    return result
