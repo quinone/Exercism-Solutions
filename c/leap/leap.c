3fbdf17cb5f7abcba27037705fedcec476869152
quinone <eoghanm.quinn@gmail.com>
2024-09-11 20:12:12 +0100

    Completed leap

--- c/leap/leap.c
+++ c/leap/leap.c
@@ -1,1 +1,14 @@
 #include "leap.h"
+
+bool leap_year(int year){
+    if ( year % 100 == 0){
+        if (year % 400 != 0){
+            return 0;
+        }     
+        return 1;
+    }   
+    if (year % 4 == 0)
+        return 1;
+    return 0;
+}
+
