c9f2634998e2027d926a09c6981d475e59289581
quinone <eoghanm.quinn@gmail.com>
2024-09-11 21:24:38 +0100

    Edit for readability

--- c/triangle/triangle.c
+++ c/triangle/triangle.c
@@ -2,17 +2,28 @@
 
 
 bool is_equilateral(triangle_t sides){
-    return (sides.a == sides.b) && (sides.a == sides.c) && sides.a && sides.b && sides.c;
+    return (sides.a == sides.b) 
+            && (sides.a == sides.c) 
+            && sides.a 
+            && sides.b 
+            && sides.c;
 }
 
 bool is_isosceles(triangle_t sides){
-    return is_triangle(sides)&&(sides.a == sides.b || sides.a == sides.c || sides.b == sides.c);
+    return is_triangle(sides)
+            && (sides.a == sides.b 
+                || sides.a == sides.c 
+                || sides.b == sides.c);
 }
 
 bool is_triangle(triangle_t sides){
-    return (sides.a + sides.b > sides.c && sides.c + sides.b > sides.a && sides.c + sides.a > sides.b); 
+    return (sides.a + sides.b > sides.c 
+            && sides.c + sides.b > sides.a 
+            && sides.c + sides.a > sides.b); 
 }
 
 bool is_scalene(triangle_t sides){
-    return is_triangle(sides) && !is_equilateral(sides) && !is_isosceles(sides);
+    return is_triangle(sides) 
+            && !is_equilateral(sides) 
+            && !is_isosceles(sides);
 }
