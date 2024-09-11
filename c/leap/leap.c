d47106f0fbe8582efb784caac7ded37c7db076f5
quinone <eoghanm.quinn@gmail.com>
2024-09-11 20:41:40 +0100

    Refactor into single return value

--- c/leap/leap.c
+++ c/leap/leap.c
@@ -1,14 +1,6 @@
 #include "leap.h"
 
 bool leap_year(int year){
-    if ( year % 100 == 0){
-        if (year % 400 != 0){
-            return 0;
-        }     
-        return 1;
-    }   
-    if (year % 4 == 0)
-        return 1;
-    return 0;
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
 
